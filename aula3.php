42974946a10c7a15e3d057b4b8404a076808c415
eversonmarquesjunior <everson.marques370@gmail.com>
2020-03-02 09:35:32 -0300

    primeiro_projeto

---
+++ aula3.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+
+</div>
+  <head>
+    <!-- <div style="background-image: url('https://cdn2.unrealengine.com/Fortnite%2Fchapter2%2FEN_11BR_KeyArt_Social-1920x1080-9d78e7401fe1b6f437143196bdb4e91c412ce23a.jpg');"> -->
+
+    <meta charset="utf-8">
+
+    <title>Juninho.com</title>
+
+    <style>
+      table, th, td {
+          border: 1px solid black;
+        }
+    </style>
+
+    <style media="screen">
+        .noticias h3 {
+        color: #1a75ff;
+        background-color: #000000;
+        font-size: 30px;
+    }
+    </style>
+
+  </head>
+
+  <body>
+
+    <h1>JR NEWS</h1>
+
+    <div class="noticias">
+      <h3>Jogador de Basquete é Viciado em Fortnite</h3>
+      <img src="https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2019/12/80066018_10162765631710461_8493602263711875072_o-338x338.jpg">
+
+      <p>Já sabemos que basquete é uma das paixões de Luka Doncic, e por isso ele se tornou tão bom no esporte que pratica. Em seu tempo livre, entretanto,
+        o prodígio não é muito diferente de outros garotos de sua geração. O jogo Fortnite é seu passatempo preferido quando não está treinando ou jogando.
+      </p>
+
+      <p>Luka confessou passar muitas horas ocupado com o jogo,
+        e até disse que estava jogando Fortnite quando foi eleito o calouro do mês de outubro em sua temporada de estreia na NBA.
+      </p>
+
+      <p>Alguns fãs, inclusive, já jogaram com Luka e chegam a postar vídeos no Youtube mostrando a atuação do astro do Mavs neste popular game.</p>
+    </div>
+
+    <div class="noticias">
+      <h3>Tabela Paulistão 2020</h3>
+      <table>
+        <tr>
+          <th>Time</th>
+          <th>Títulos</th>
+          <th>Rebaixamentos</th>
+        </tr>
+
+        <tr>
+          <td>Santos</td>
+          <td>68</td>
+          <td>0</td>
+        </tr>
+
+        <tr>
+          <td>Curintia</td>
+          <td>1</td>
+          <td>15</td>
+        </tr>
+
+        <tr>
+          <td>Parmeira</td>
+          <td>0</td>
+          <td>87</td>
+        </tr>
+
+      </table>
+      <p>Depois de uma semana de incertezas e pressão, o técnico Jesualdo Ferreira respira aliviado.</p>
+      <p>Neste sábado, o Santos empatou por 0 a 0 com o Palmeiras, no Pacaembu,
+        pelo Campeonato Paulista, mostrou evolução em campo e ganhou confiança para a Libertadores.</p>
+
+    </div>
+
+    <div class="noticias">
+      <h3>Nike Air Mag: R$ 277,7 mil</h3>
+      <img src="https://conteudo.imguol.com.br/c/entretenimento/88/2016/10/05/tenis-nike-air-mag-inspirado-no-filme-de-volta-para-o-futuro-2-1475685829665_560x400.jpg" >
+
+      <p>O tênis de "De Volta para o Futuro" com cadarços que se amarram sozinhos foi lançado no ano passado e apenas 89 unidades foram sorteadas pela Nike. No ano passado, uma delas foi vendida por 200 mil dólares em um leilão beneficente em Nova York,
+        para a fundação Michael J. Fox de pesquisa sobre o Mal de Parkinson. A versão sendo leilado hoje, porém, não tem, aparentemente, fins beneficentes
+      </p>
+
+    </div>
+    <br>
+    <br>
+    <br>
+  </div>
+  </body>
+</html>
