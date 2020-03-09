68bd973302df16042de1a28ea54173fe72357ab9
eversonmarquesjunior <everson.marques370@gmail.com>
2020-03-09 09:32:24 -0300

    aulacss

---
+++ aula4.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>AULA CSS e HTML</title>
+    <!-- Importação de CSS é feita no HEAD -->
+    <link href="https://fonts.googleapis.com/css?family=Patrick+Hand+SC|Sedgwick+Ave+Display&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="aula4.css">
+  </head>
+
+  <body>
+    <div class="site">
+
+      <div class="topo">
+        <h3>Topo</h3>
+      </div>
+
+      <div class="principal">
+        Corpo
+      </div>
+
+      <div class="rodape">
+        Desenvolvido por Juninho
+      </div>
+
+    </div>
+
+  </body>
+
+</html>
