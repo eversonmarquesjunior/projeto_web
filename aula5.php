1f385b64fb412841e35319eb37117704cd46acb7
eversonmarquesjunior <everson.marques370@gmail.com>
2020-03-16 09:57:10 -0300

    menuebotao

---
+++ aula5.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>MENU E BOTÕES</title>
+    <!-- Importação de CSS é feita no HEAD -->
+    <link href="https://fonts.googleapis.com/css?family=Patrick+Hand+SC|Sedgwick+Ave+Display&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="aula5.css">
+  </head>
+
+  <body>
+    <div class="site">
+
+      <div class="topo">
+        <h3>Topo</h3>
+
+        <div class="menu">
+
+          <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Contato</a></li>
+              <li><a href="#">Cadastro</a></li>
+              <li><a href="#">Ajuda</a></li>
+              <li><a href="#">Login</a></li>
+          </ul>
+
+        </div>
+
+      </div>
+
+      <div class="principal">
+        <h3>JR NEWS</h3>
+
+        <div class="noticia1">
+          <h3>Jogador de Basquete é Viciado em Fortnite</h3>
+          <!-- <img src="https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2019/12/80066018_10162765631710461_8493602263711875072_o-338x338.jpg"> -->
+          <img id="img" src="https://clutchpoints.com/wp-content/uploads/2018/12/Luka-Doncic-admits-he_s-playing-Fortnite-when-he-found-out-he-won-Rookie-of-the-Month.jpg">
+
+          <p>Já sabemos que basquete é uma das paixões de Luka Doncic, e por isso ele se tornou tão bom no esporte que pratica. Em seu tempo livre, entretanto,
+            o prodígio não é muito diferente de outros garotos de sua geração.
+
+          O jogo Fortnite é seu passatempo preferido quando não está treinando ou jogando.
+
+
+          Luka confessou passar muitas horas ocupado com o jogo,
+          e até disse que estava jogando Fortnite quando foi eleito o calouro do mês de outubro em sua temporada de estreia na NBA.
+
+          Alguns fãs, inclusive, já jogaram com Luka e chegam a postar vídeos no Youtube mostrando a atuação do astro do Mavs neste popular game.</p>
+        </div>
+
+        <h2>Botões</h2>
+        <a href="#" class="botao botao-azul">Botão Azul</a>
+        <a href="#" class="botao botao-verde">Botão Verde</a>
+        <a href="#" class="botao botao-vermelho">Botão Vermelho</a>
+
+      </div>
+
+      <div class="rodape">
+        <h3>Desenvolvido por Juninho</h3>
+      </div>
+
+    </div>
+
+  </body>
+
+</html>
